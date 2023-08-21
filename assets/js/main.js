414c25bb7036d6fd66e6808136d4402bfffe10b5
web-ogs <142250624+web-ogs@users.noreply.github.com>
2023-08-21 11:41:03 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,10 +11,6 @@
 # General
 --------------------------------------------------------------*/
   
-  
-
-
-
     /**
  * Easy selector helper function
  */
@@ -47,12 +43,6 @@
     const onscroll = (el, listener) => {
         el.addEventListener('scroll', listener)
     }
-
-
-
-
-
-
 
 
     /**
@@ -128,6 +118,24 @@
     }
 
 
+    /**
+     * Mobile nav toggle
+     */
+    on('click', '.mobile-nav-toggle', function (e) {
+        select('#navbar').classList.toggle('navbar-mobile')
+        this.classList.toggle('bi-list')
+        this.classList.toggle('bi-x')
+    })
+
+    /**
+     * Mobile nav dropdowns activate
+     */
+    on('click', '.navbar .dropdown > a', function (e) {
+        if (select('#navbar').classList.contains('navbar-mobile')) {
+            e.preventDefault()
+            this.nextElementSibling.classList.toggle('dropdown-active')
+        }
+    }, true)
 
 
     /**
@@ -159,6 +167,17 @@
         }
     });
 
+    /**
+ * Preloader
+ */
+    let preloader = select('#preloader');
+    if (preloader) {
+        window.addEventListener('load', () => {
+            setTimeout(() => {
+                preloader.remove()
+            }, 100);
+        });
+    }
 
     /**
      * Hero type effect
@@ -198,76 +217,20 @@
     /*--------------------------------------------------------------
 # Hover
 --------------------------------------------------------------*/
-   
+    
+    const text = baffle(".codedText");
+    text.set({
+        characters: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456890;[]\,./><&%!*',
+        speed: 50
+    });
+    text.start();
+        text.reveal(3000);
+
+  
 
 
-    window.onload = function () {
-        const links = document.querySelectorAll("codedText");
-        const solveMilliseconds = 800;
-        const characterSelectionMilliseconds = 40;
-        const delayMilliseconds = 250;
-        const characters = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890*#@/*!%&^"];
-
-        const randomArrayElement = (arr) => {
-            return arr[(arr.length * Math.random()) | 0];
-        };
-
-        links.forEach((element) => {
-            element.addEventListener("mouseenter", (e) => {
-                const element = e.target;
-                scrambleText(element);
-                e.preventDefault();
-            });
-        });
-
-        function scrambleText(element) {
-            if (element.classList.contains("active") == false) {
-                let delay = 0;
-                const elementText = element.innerText;
-                const elementCharacters = [...elementText];
-                const lockMilliseconds =
-                    delayMilliseconds * elementCharacters.length + solveMilliseconds;
-
-                element.classList.add("active");
-
-                setTimeout(() => {
-                    element.classList.remove("active");
-                }, lockMilliseconds);
-
-                elementCharacters.forEach((character, index) => {
-                    setTimeout(
-                        () => {
-                            let intervalId = setInterval(() => {
-                                const randomCharacter = randomArrayElement(characters);
-                                element.innerText = replaceCharacter(
-                                    element.innerText,
-                                    index,
-                                    randomCharacter
-                                );
-
-                                setTimeout(() => {
-                                    clearInterval(intervalId);
-                                    element.innerText = replaceCharacter(
-                                        element.innerText,
-                                        index,
-                                        elementCharacters[index]
-                                    );
-                                }, solveMilliseconds);
-                            }, characterSelectionMilliseconds);
-                        },
-                        delay === 0 ? (delay += 1) : (delay += delayMilliseconds)
-                    );
-                });
-            }
-        }
-
-        function replaceCharacter(str, index, chr) {
-            return `${str.substring(0, index)}${chr}${str.substring(index + 1)}`;
-        }
-    };
 
 
-   
   
 
     /**
