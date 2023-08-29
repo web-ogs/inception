ff75f4f248ca75c33aa81f9d0927a7ec56d4c186
web-ogs <142250624+web-ogs@users.noreply.github.com>
2023-08-29 15:35:50 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,107 +3,223 @@
 * Author: Courtney - web-OGs
 */
 
+/** TIME DISPLAY */
+`use strict`;
+function refreshTime() {
+    var datetime = new Date().toLocaleTimeString();
+    console.log(datetime); // it will represent date in the console of developers tool
+    document.getElementById("time").textContent = datetime; // represent on html page
+}
+setInterval(refreshTime, 100);
+
+
+/** CODEDTEXT HOVER */
+document.querySelectorAll('.codedText').forEach((t) => {
+    const arr1 = t.innerHTML.split('')
+    const arr2 = []
+    arr1.forEach((char, i) => arr2[i] = randChar()) //fill arr2 with random characters
+    t.onpointerover = () => {
+        const tl = gsap.timeline()
+        let step = 0
+        tl.fromTo(t, {
+            innerHTML: arr2.join(''),
+          
+            
+        }, {
+            duration: arr1.length / 20, //duration based on text length
+            ease: 'power1.in',
+            delay: 0.1,
+        
+           
+            onUpdate: () => {
+                const p = Math.floor(tl.progress() * (arr1.length)) //whole number from 0 - text length
+                if (step != p) { //throttle the change of random characters
+                    step = p
+                    arr1.forEach((char, i) => arr2[i] = randChar())
+                    let pt1 = arr1.join('').substring(p, 0),
+                        pt2 = arr2.join('').substring(arr2.length - p, 0)
+                    if (t.classList.contains('fromRight')) {
+                        pt1 = arr2.join('').substring(arr2.length - p, 0)
+                        pt2 = arr1.join('').substring(arr1.length - p)
+                    }
+                    t.innerHTML = pt1 + pt2 //update text
+                }
+
+            }
+        })
+    }
+})
+
+function randChar() {
+    let c = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$^&*()…æ_+-=;[]/~`"
+    c = c[Math.floor(Math.random() * c.length)]
+    return (Math.random() > 0.5) ? c : c.toUpperCase()
+}
+
+/** MODAL CLICK */
+// Get the modal
+var modal1 = document.getElementById("myModal1");
+
+// Get the button/div that opens the modal
+var div = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close1")[0];
+
+// When the user clicks the button, open the modal 
+div.onclick = function () {
+    modal1.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal1.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal1.style.display = "none";
+    }
+}
+/** MODAL 2 CLICK */
+// Get the modal
+var modal2 = document.getElementById("myModal2");
+
+// Get the button/div that opens the modal
+var div2 = document.getElementById("myBtn2");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close2")[0];
+
+// When the user clicks the button, open the modal 
+div2.onclick = function () {
+    modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+}
+
+/** MODAL 3 CLICK */
+// Get the modal
+var modal3 = document.getElementById("myModal3");
+
+// Get the button/div that opens the modal
+var div3 = document.getElementById("myBtn3");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close3")[0];
+
+// When the user clicks the button, open the modal 
+div3.onclick = function () {
+    modal3.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal3.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal3) {
+        modal3.style.display = "none";
+    }
+}
+
+/** DROPDOWN SOCIALS CLICK */
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};
+
+
+
+/** SQAURE ANIMATION */
+$(window).scroll(function () {
+    var w = $(window).scrollTop();
+
+    var f = "translateX(" + w * 0.45 + "px)";
+
+    $('.box.three').css({
+        transform: f
+    })
+
+   
+
+});
+
+
+
+
+
+
+
+/** PAGE LOAD */
 
 (function () {
     "use strict";
 
-/*--------------------------------------------------------------
-# General
---------------------------------------------------------------*/
-  
-    /**
- * Easy selector helper function
+
+
+/**
+* Easy selector helper function
+*/
+const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+        return [...document.querySelectorAll(el)]
+    } else {
+        return document.querySelector(el)
+    }
+}
+/**
+ * Easy event listener function
  */
-    const select = (el, all = false) => {
-        el = el.trim()
+const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
         if (all) {
-            return [...document.querySelectorAll(el)]
+            selectEl.forEach(e => e.addEventListener(type, listener))
         } else {
-            return document.querySelector(el)
+            selectEl.addEventListener(type, listener)
         }
     }
+}
+/**
+ * Easy on scroll event listener 
+ */
+const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+}
 
     /**
-     * Easy event listener function
-     */
-    const on = (type, el, listener, all = false) => {
-        let selectEl = select(el, all)
-        if (selectEl) {
-            if (all) {
-                selectEl.forEach(e => e.addEventListener(type, listener))
-            } else {
-                selectEl.addEventListener(type, listener)
-            }
-        }
-    }
-
-    /**
-     * Easy on scroll event listener 
-     */
-    const onscroll = (el, listener) => {
-        el.addEventListener('scroll', listener)
-    }
-
-
-    /**
-     * Navbar links active state on scroll
-     */
-    let navbarlinks = select('#navbar .scrollto', true)
-    const navbarlinksActive = () => {
-        let position = window.scrollY + 300
-        navbarlinks.forEach(navbarlink => {
-            if (!navbarlink.hash) return
-            let section = select(navbarlink.hash)
-            if (!section) return
-            if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-                navbarlink.classList.add('active')
-            } else {
-                navbarlink.classList.remove('active')
-            }
-        })
-    }
-    window.addEventListener('load', navbarlinksActive)
-    onscroll(document, navbarlinksActive)
-
-    /**
-     * Scrolls to an element with header offset
-     */
-    const scrollto = (el) => {
-        let header = select('#header')
-        let offset = header.offsetHeight
-
-        let elementPos = select(el).offsetTop
-        window.scrollTo({
-            top: elementPos - offset,
-            behavior: 'smooth'
-        })
-    }
-
-    /**
-     * Header fixed top on scroll
-     */
-    let selectHeader = select('#header')
-    if (selectHeader) {
-        let headerOffset = selectHeader.offsetTop
-        let nextElement = selectHeader.nextElementSibling
-        const headerFixed = () => {
-            if ((headerOffset - window.scrollY) <= 0) {
-                selectHeader.classList.add('fixed-top')
-                nextElement.classList.add('scrolled-offset')
-            } else {
-                selectHeader.classList.remove('fixed-top')
-                nextElement.classList.remove('scrolled-offset')
-            }
-        }
-        window.addEventListener('load', headerFixed)
-        onscroll(document, headerFixed)
-    }
-
-
-
-    /**
-     * Back to top button
-     */
+* Back to top button
+*/
     let backtotop = select('.back-to-top')
     if (backtotop) {
         const toggleBacktotop = () => {
@@ -118,116 +234,31 @@
     }
 
 
-
-    /**
-     * Scrool with ofset on links with a class name .scrollto
-     */
-    on('click', '.scrollto', function (e) {
-        if (select(this.hash)) {
-            e.preventDefault()
-
-            let navbar = select('#navbar')
-            if (navbar.classList.contains('navbar-mobile')) {
-                navbar.classList.remove('navbar-mobile')
-                let navbarToggle = select('.mobile-nav-toggle')
-                navbarToggle.classList.toggle('bi-list')
-                navbarToggle.classList.toggle('bi-x')
-            }
-            scrollto(this.hash)
-        }
-    }, true)
-
-    /**
-     * Scroll with ofset on page load with hash links in the url
-     */
-    window.addEventListener('load', () => {
-        if (window.location.hash) {
-            if (select(window.location.hash)) {
-                scrollto(window.location.hash)
-            }
+/** LINK STATES ON SCROLL */
+let section = document.querySelectorAll('section');
+let menu = document.querySelectorAll('header nav a');
+window.onscroll = () => {
+    section.forEach(i => {
+        let top = window.scrollY;
+        let offset = i.offsetTop - 350;
+        let height = i.offsetHeight;
+        let id = i.getAttribute('id');
+        if (top >= offset && top < offset + height) {
+            menu.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']')
+                    .classList.add('active');
+            });
         }
     });
-
-    /**
- * Preloader
- */
-    let preloader = select('#preloader');
-    if (preloader) {
-        window.addEventListener('load', () => {
-            setTimeout(() => {
-                preloader.remove()
-            }, 2500);
-        });
-    }
+};
+window.addEventListener('load', navbarlinksActive)
+onscroll(document, navbarlinksActive)
 
 
-
-    /**
-     * Hero type effect
-     */
-    const typed = select('.typed')
-    if (typed) {
-        let typed_strings = typed.getAttribute('data-typed-items')
-        typed_strings = typed_strings.split(',')
-        new Typed('.typed', {
-            strings: typed_strings,
-            loop: true,
-            typeSpeed: 100,
-            backSpeed: 50,
-            backDelay: 2000
-        });
-    }   
-
-
-
-
-    /**
-     * Animation on scroll
-     */
-    function aos_init() {
-        AOS.init({
-            duration: 1000,
-            easing: "ease-in-out",
-            once: true,
-            mirror: false
-        });
-    }
-    window.addEventListener('load', () => {
-        aos_init();
-    });
-
-
-
-
-
-    /*--------------------------------------------------------------
-# decode
---------------------------------------------------------------*/
-
-
-
-    const text = baffle(".codedText");
-    text.set({
-        characters: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456890;[]\,./><&%!*',
-        speed: 50
-    });
-    text.start();
-    text.reveal(8500);
-
-
-
-    const maintext = baffle(".maincodedText");
-    maintext.set({
-        characters: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456890;[]\,./><&%!*',
-        speed: 85
-    });
-    maintext.start();
-    maintext.reveal(6500);
-
-
-
-    /**
-     * END
-     */
+   
 })()
+
+
+
 
