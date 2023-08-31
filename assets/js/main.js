d6e182aff368654108be3aefea6746278b0fe6a1
web-ogs <142250624+web-ogs@users.noreply.github.com>
2023-08-31 16:10:38 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,101 @@
 * Author: Courtney - web-OGs
 */
 
+var Messenger = function (el) {
+    'use strict';
+    var m = this;
+
+    m.init = function () {
+        m.codeletters = "&#*+%?£@§$";
+        m.message = 0;
+        m.current_length = 0;
+        m.fadeBuffer = false;
+        m.messages = [
+            'Loading...',
+            'Ready_'
+        ];
+
+        setTimeout(m.animateIn, 10);
+    };
+
+    m.generateRandomString = function (length) {
+        var random_text = '';
+        while (random_text.length < length) {
+            random_text += m.codeletters.charAt(Math.floor(Math.random() * m.codeletters.length));
+        }
+
+        return random_text;
+    };
+
+    m.animateIn = function () {
+        if (m.current_length < m.messages[m.message].length) {
+            m.current_length = m.current_length + 2;
+            if (m.current_length > m.messages[m.message].length) {
+                m.current_length = m.messages[m.message].length;
+            }
+
+            var message = m.generateRandomString(m.current_length);
+            $(el).html(message);
+
+            setTimeout(m.animateIn, 20);
+        } else {
+            setTimeout(m.animateFadeBuffer, 20);
+        }
+    };
+
+    m.animateFadeBuffer = function () {
+        if (m.fadeBuffer === false) {
+            m.fadeBuffer = [];
+            for (var i = 0; i < m.messages[m.message].length; i++) {
+                m.fadeBuffer.push({ c: (Math.floor(Math.random() * 12)) + 1, l: m.messages[m.message].charAt(i) });
+            }
+        }
+
+        var do_cycles = false;
+        var message = '';
+
+        for (var i = 0; i < m.fadeBuffer.length; i++) {
+            var fader = m.fadeBuffer[i];
+            if (fader.c > 0) {
+                do_cycles = true;
+                fader.c--;
+                message += m.codeletters.charAt(Math.floor(Math.random() * m.codeletters.length));
+            } else {
+                message += fader.l;
+            }
+        }
+
+        $(el).html(message);
+
+        if (do_cycles === true) {
+            setTimeout(m.animateFadeBuffer, 50);
+        } else {
+            setTimeout(m.cycleText, 2000);
+        }
+    };
+
+    m.cycleText = function () {
+        m.message = m.message + 1;
+        if (m.message >= m.messages.length) {
+            m.message = 0;
+        }
+
+        m.current_length = 0;
+        m.fadeBuffer = false;
+        $(el).html('');
+
+        setTimeout(m.animateIn, 200);
+    };
+
+    m.init();
+}
+
+console.clear();
+var messenger = new Messenger($('#messenger'));
+
+
+
+
 /** TIME DISPLAY */
 `use strict`;
 function refreshTime() {
@@ -55,6 +150,53 @@ function randChar() {
     c = c[Math.floor(Math.random() * c.length)]
     return (Math.random() > 0.5) ? c : c.toUpperCase()
 }
+
+
+
+/** CODEDTEXT HOVER */
+document.querySelectorAll('.contact-codedText').forEach((t) => {
+    const arr1 = t.innerHTML.split('')
+    const arr2 = []
+    arr1.forEach((char, i) => arr2[i] = randChar()) //fill arr2 with random characters
+    t.onpointerover = () => {
+        const tl = gsap.timeline()
+        let step = 0
+        tl.fromTo(t, {
+            innerHTML: arr2.join(''),
+
+
+        }, {
+            duration: arr1.length / 20, //duration based on text length
+            ease: 'power1.in',
+            delay: 0.1,
+
+
+            onUpdate: () => {
+                const p = Math.floor(tl.progress() * (arr1.length)) //whole number from 0 - text length
+                if (step != p) { //throttle the change of random characters
+                    step = p
+                    arr1.forEach((char, i) => arr2[i] = randChar())
+                    let pt1 = arr1.join('').substring(p, 0),
+                        pt2 = arr2.join('').substring(arr2.length - p, 0)
+                    if (t.classList.contains('fromRight')) {
+                        pt1 = arr2.join('').substring(arr2.length - p, 0)
+                        pt2 = arr1.join('').substring(arr1.length - p)
+                    }
+                    t.innerHTML = pt1 + pt2 //update text
+                }
+
+            }
+        })
+    }
+})
+
+function randChar() {
+    let c = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$^&*()…æ_+-=;[]/~`"
+    c = c[Math.floor(Math.random() * c.length)]
+    return (Math.random() > 0.5) ? c : c.toUpperCase()
+}
+
+
 
 /** MODAL CLICK */
 // Get the modal
@@ -159,22 +301,40 @@ window.onclick = function (event) {
 
 
 
+/** HERO TEXT ANIMATION */
+$(window).scroll(function () {
+    var a = $(window).scrollTop();
+    var b = "translateX(" + a * 0.35 + "px)";
+    var c = "translateX(" + -a * 0.25 + "px)";
+    var d = "translateX(" + a * 0.25 + "px)";
+
+    $('.hero-text-1').css({
+        transform: b
+    });
+    $('.hero-text-2').css({
+        transform: c
+    });
+    $('.hero-text-3').css({
+        transform: d
+    })
+});
+
+
 /** SQAURE ANIMATION */
 $(window).scroll(function () {
     var w = $(window).scrollTop();
 
-    var f = "translateX(" + w * 0.45 + "px)";
+    var f = "translateX(" + w * 0.12 + "px)";
+    var f1 = "translateX(" + w * 0.30 + "px)";
 
     $('.box.three').css({
         transform: f
-    })
-
-   
+    });
+    $('.box1.three1').css({
+        transform: f1
+    }) 
 
 });
-
-
-
 
 
 
@@ -217,24 +377,9 @@ const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
 }
 
+
+    /** LINK STATES ON SCROLL */
     /**
-* Back to top button
-*/
-    let backtotop = select('.back-to-top')
-    if (backtotop) {
-        const toggleBacktotop = () => {
-            if (window.scrollY > 100) {
-                backtotop.classList.add('active')
-            } else {
-                backtotop.classList.remove('active')
-            }
-        }
-        window.addEventListener('load', toggleBacktotop)
-        onscroll(document, toggleBacktotop)
-    }
-
-
-/** LINK STATES ON SCROLL */
 let section = document.querySelectorAll('section');
 let menu = document.querySelectorAll('header nav a');
 window.onscroll = () => {
@@ -253,9 +398,90 @@ window.onscroll = () => {
     });
 };
 window.addEventListener('load', navbarlinksActive)
-onscroll(document, navbarlinksActive)
+    onscroll(document, navbarlinksActive)
+        * /
 
 
+    /**
+     * Navbar links active state on scroll
+     */
+    let navbarlinks = select('#navbar .scrollto', true)
+    const navbarlinksActive = () => {
+        let position = window.scrollY + 500
+        navbarlinks.forEach(navbarlink => {
+            if (!navbarlink.hash) return
+            let section = select(navbarlink.hash)
+            if (!section) return
+            if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+                navbarlink.classList.add('active')
+            } else {
+                navbarlink.classList.remove('active')
+            }
+        })
+    }
+    window.addEventListener('load', navbarlinksActive)
+    onscroll(document, navbarlinksActive)
+
+
+
+
+    /**
+ * Scrolls to an element with header offset
+ */
+    const scrollto = (el) => {
+        let header = select('#header-top')
+        let offset = header.offsetHeight
+
+        let elementPos = select(el).offsetTop
+        window.scrollTo({
+            top: elementPos - offset,
+            behavior: 'smooth'
+        })
+    }
+
+
+    /**
+* Back to top button
+*/
+    let backtotop = select('.back-to-top')
+    if (backtotop) {
+        const toggleBacktotop = () => {
+            if (window.scrollY > 100) {
+                backtotop.classList.add('active')
+            } else {
+                backtotop.classList.remove('active')
+            }
+        }
+        window.addEventListener('load', toggleBacktotop)
+        onscroll(document, toggleBacktotop)
+    }
+
+    /**
+* Preloader
+*/
+    let preloader = select('#preloader-page');
+    if (preloader) {
+        window.addEventListener('load', () => {
+            setTimeout(() => {
+                preloader.remove()
+            }, 5000);
+        });
+    }
+
+
+
+
+
+    /**
+     * Scroll with ofset on page load with hash links in the url
+     */
+    window.addEventListener('load', () => {
+        if (window.location.hash) {
+            if (select(window.location.hash)) {
+                scrollto(window.location.hash)
+            }
+        }
+    });
    
 })()
 
