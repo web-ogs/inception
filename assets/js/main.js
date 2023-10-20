0d735b0c6f93de8e9de8447a26cdf2686e0971d6
web-ogs <142250624+web-ogs@users.noreply.github.com>
2023-10-20 16:04:12 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -486,23 +486,6 @@ const onscroll = (el, listener) => {
 
 
 
-
-    /**
- * Scrolls to an element with header offset
- */
-    const scrollto = (el) => {
-        let header = select('#header-top')
-        let offset = header.offsetHeight
-
-        let elementPos = select(el).offsetTop
-        window.scrollTo({
-            top: elementPos - offset,
-            behavior: 'smooth'
-        })
-    }
-
-
-
     /**
 * Back to top button
 */
@@ -583,33 +566,7 @@ const onscroll = (el, listener) => {
         }
     });
 
- /**
-     * Smooth scroll
-     */
-    $(document).ready(function(){
-        // Add smooth scrolling to all links
-        $("a").on('click', function(event) {
-      
-          // Make sure this.hash has a value before overriding default behavior
-          if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
-      
-            // Store hash
-            var hash = this.hash;
-      
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-              scrollTop: $(hash).offset().top
-            }, 500, function(){
-      
-              // Add hash (#) to URL when done scrolling (default click behavior)
-              window.location.hash = hash;
-            });
-          } // End if
-        });
-      });
+
 
 
 
